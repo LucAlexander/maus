1a99462134f0e3dcea00d20b4324ba5e250124c7
Luc Alexander <lucskyalex@gmail.com>
2025-11-03 11:48:22 +0100

    error logging

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const Buffer = std.ArrayList;
 
+const Error = struct {
+	message: []u8,
+	pos: u64
+};
+
 pub fn main() !void {
 	const allocator = std.heap.page_allocator;
 	var main_mem = std.heap.ArenaAllocator.init(allocator);
@@ -20,11 +25,16 @@ pub fn main() !void {
 		return;
 	};
 	defer allocator.free(contents);
-	const program = parse(&mem, contents) catch |err| {
-		std.debug.print("Error in parse stream: {}\n", .{err});
-		return;
-	};
-	show_program(program);
+	var error_log = Buffer(Error).init(mem);
+	const program = parse(&mem, contents, &error_log);
+	if (error_log.items.len != 0){
+		for (error_log.items) |err| {
+			show_error(contents, err);
+		}
+	}
+	else{
+		show_program(program);
+	}
 }
 
 const Part = union(enum) {
@@ -57,7 +67,7 @@ const ParseError = error {
 	UnexpectedEOF
 };
 
-pub fn parse(mem: *const std.mem.Allocator, text: []u8) ParseError!Program {
+pub fn parse(mem: *const std.mem.Allocator, text: []u8, err: *Buffer(Error)) Program {
 	var i: u64 = 0;
 	var program = Buffer(Bind).init(mem.*);
 	outer: while (i<text.len) : (i += 1) {
@@ -69,15 +79,19 @@ pub fn parse(mem: *const std.mem.Allocator, text: []u8) ParseError!Program {
 			}
 			c = text[i];
 		}
-		const left = try parse_left(mem, &i, text);
-		const bind = try parse_right(mem, &i, text, left);
+		const left = parse_left(mem, &i, text, err) catch {
+			continue;
+		};
+		const bind = parse_right(mem, &i, text, left, err) catch {
+			continue;
+		};
 		program.append(bind)
 			catch unreachable;
 	}
 	return program;
 }
 
-pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buffer(Part)) ParseError!Bind {
+pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buffer(Part), err: *Buffer(Error)) ParseError!Bind {
 	var current_alt = Buffer(Part).init(mem.*);
 	var current_bind = Bind.init(mem);
 	current_bind.left = left;
@@ -92,11 +106,13 @@ pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buf
 		}
 		if (c == '='){
 			if (current_bind.right.items.len != 0){
-				std.debug.print("Expected alternate for equation right hand side, found =\n", .{});
+				err.append(set_error(mem, i.*, "Expected alternate for equation right hand side, found =\n", .{}))
+					catch unreachable;
 				return ParseError.UnexpectedToken;
 			}
 			if (current_alt.items.len == 0){
-				std.debug.print("Expected right hand side for equation, found =\n", .{});
+				err.append(set_error(mem, i.*, "Expected right hand side for equation, found =\n", .{}))
+					catch unreachable;
 				return ParseError.UnexpectedToken;
 			}
 			i.* += 1;
@@ -107,7 +123,7 @@ pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buf
 					catch unreachable;
 				current_bind.subbinds.?.* = Buffer(Bind).init(mem.*);
 			}
-			current_bind.subbinds.?.append(try parse_right(mem, i, text, current_alt))
+			current_bind.subbinds.?.append(try parse_right(mem, i, text, current_alt, err))
 				catch unreachable;
 			current_alt = Buffer(Part).init(mem.*);
 			continue;
@@ -126,7 +142,7 @@ pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buf
 		}
 		else if (c == '('){
 			i.* += 1;
-			const comp = try parse_comp_ref(mem, i, text);
+			const comp = try parse_comp_ref(mem, i, text, err);
 			std.debug.assert(text[i.*] == ')');
 			const part = Part{
 				.compref = mem.create(Buffer(Part)) catch unreachable
@@ -170,7 +186,7 @@ pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buf
 			end += 1;
 			if (text[end] == '('){
 				end += 1;
-				const comp = try parse_comp_ref(mem, &end, text);
+				const comp = try parse_comp_ref(mem, &end, text, err);
 				std.debug.assert(text[end] == ')');
 				const judge = Part{
 					.compref = mem.create(Buffer(Part)) catch unreachable
@@ -203,10 +219,12 @@ pub fn parse_right(mem: *const std.mem.Allocator, i: *u64, text: []u8, left: Buf
 		current_alt.append(part)
 			catch unreachable;
 	}
+	err.append(set_error(mem, i.*, "Encountered end of file while parsing right hand side of equation \n", .{}))
+		catch unreachable;
 	return ParseError.UnexpectedEOF;
 }
 
-pub fn parse_comp_ref(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError!Buffer(Part) {
+pub fn parse_comp_ref(mem: *const std.mem.Allocator, i: *u64, text: []u8, err: *Buffer(Error)) ParseError!Buffer(Part) {
 	var current_alt = Buffer(Part).init(mem.*);
 	outer: while (i.* < text.len) : (i.* += 1){
 		var c = text[i.*];
@@ -219,13 +237,15 @@ pub fn parse_comp_ref(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseE
 		}
 		if (c == ')'){
 			if (current_alt.items.len == 0){
+				err.append(set_error(mem, i.*, "Encountered empty nested expression in equation\n", .{}))
+					catch unreachable;
 				return ParseError.UnexpectedToken;
 			}
 			return current_alt;
 		}
 		else if (c == '('){
 			i.* += 1;
-			const comp = try parse_comp_ref(mem, i, text);
+			const comp = try parse_comp_ref(mem, i, text, err);
 			std.debug.assert(text[i.*] == ')');
 			const part = Part{
 				.compref = mem.create(Buffer(Part)) catch unreachable
@@ -269,7 +289,7 @@ pub fn parse_comp_ref(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseE
 			end += 1;
 			if (text[end] == '('){
 				end += 1;
-				const comp = try parse_comp_ref(mem, &end, text);
+				const comp = try parse_comp_ref(mem, &end, text, err);
 				std.debug.assert(text[end] == ')');
 				const judge = Part{
 					.compref = mem.create(Buffer(Part)) catch unreachable
@@ -302,10 +322,12 @@ pub fn parse_comp_ref(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseE
 		current_alt.append(part)
 			catch unreachable;
 	}
+	err.append(set_error(mem, i.*, "Encountered end of file while parsing right hand nested equation\n", .{}))
+		catch unreachable;
 	return ParseError.UnexpectedEOF;
 }
 
-pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError!Buffer(Part) {
+pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8, err: *Buffer(Error)) ParseError!Buffer(Part) {
 	var current_alt = Buffer(Part).init(mem.*);
 	outer: while (i.* < text.len) : (i.* += 1){
 		var c = text[i.*];
@@ -318,6 +340,8 @@ pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError
 		}
 		if (c == '='){
 			if (current_alt.items.len == 0){
+				err.append(set_error(mem, i.*, "Encountered empty left hand side of equation\n", .{}))
+					catch unreachable;
 				return ParseError.UnexpectedToken;
 			}
 			i.* += 1;
@@ -325,7 +349,7 @@ pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError
 		}
 		else if (c == '('){
 			i.* += 1;
-			const comp = try parse_comp_ref(mem, i, text);
+			const comp = try parse_comp_ref(mem, i, text, err);
 			std.debug.assert(text[i.*] == ')');
 			const part = Part{
 				.compref = mem.create(Buffer(Part)) catch unreachable
@@ -369,7 +393,7 @@ pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError
 			end += 1;
 			if (text[end] == '('){
 				end += 1;
-				const comp = try parse_comp_ref(mem, &end, text);
+				const comp = try parse_comp_ref(mem, &end, text, err);
 				std.debug.assert(text[end] == ')');
 				const judge = Part{
 					.compref = mem.create(Buffer(Part)) catch unreachable
@@ -402,6 +426,8 @@ pub fn parse_left(mem: *const std.mem.Allocator, i: *u64, text: []u8) ParseError
 		current_alt.append(part)
 			catch unreachable;
 	}
+	err.append(set_error(mem, i.*, "Encountered end of file while parsing left side of equation\n", .{}))
+		catch unreachable;
 	return ParseError.UnexpectedEOF;
 }
 
@@ -452,4 +478,69 @@ pub fn show_part(part: Part) void {
 			std.debug.print(") ", .{});
 		}
 	}
+}
+
+pub fn set_error(mem: *const std.mem.Allocator, index:u64, comptime fmt: []const u8, args: anytype) Error {
+	var err = Error{
+		.pos = index,
+		.message = mem.alloc(u8, 128) catch unreachable,
+	};
+	const result = std.fmt.bufPrint(err.message, fmt, args)
+		catch unreachable;
+	err.message.len = result.len;
+	return err;
+}
+
+pub fn show_error(text: []u8, err: Error) void {
+	var i: u64 = 0;
+	const dist = 32;
+	var start_pos:u64 = 0;
+	var end_pos:u64 = text.len;
+	var found_start = false;
+	var line:u64 = 1;
+	var start_line:u64 = 1;
+	while (i < text.len){
+		if (text[i] == '\n'){
+			line += 1;
+		}
+		if (i>err.pos-dist and !found_start){
+			if (text[i] == '\n'){
+				start_pos = i+1;
+				found_start = true;
+				start_line = line;
+			}
+		}
+		if (i > err.pos+dist){
+			if (text[i] == '\n'){
+				end_pos = i;
+				break;
+			}
+		}
+		i += 1;
+	}
+	line = start_line;
+	const stderr = std.io.getStdErr().writer();
+	stderr.print("\x1b[1m{s}\x1b[0m\n", .{err.message})
+		catch unreachable;
+	stderr.print("{d:06} | ", .{line})
+		catch unreachable;
+	for (start_pos .. end_pos) |k| {
+		if (text[k] == '\n'){
+			line += 1;
+			stderr.print("\n{d:06} | ", .{line})
+				catch unreachable;
+			continue;
+		}
+		if (k == err.pos){
+			stderr.print("\x1b[1;4;31m", .{})
+				catch unreachable;
+		}
+		std.debug.print("{c}", .{text[k]});
+		if (k == err.pos){
+			stderr.print("\x1b[0m", .{})
+				catch unreachable;
+		}
+	}
+	stderr.print("\n", .{})
+		catch unreachable;
 }
